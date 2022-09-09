f382cfa0bd029360db2751ebbd665bada6fddd2f
sitaram-mulik <sitaram99.race@gmail.com>
2022-09-10 00:41:39 +0530

    feat: test husky

--- src/app/modules/core/components/header/header.component.ts
+++ src/app/modules/core/components/header/header.component.ts
@@ -7,8 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
     ngOnInit(): void {
-        const log: unknown = 'HI';
-        const m: unknown = 10;
+        const log: unknown = 'Hello';
         console.log(log);
     }
 }
